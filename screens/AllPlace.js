0b3360c9646ae7370b7c6b70451f1c9b60a74ce5
Hüseyin Koç <58426496+hsynkc00@users.noreply.github.com>
2023-09-22 16:38:40 +0300

    Screens and components are created.

---
+++ screens/AllPlace.js
@@ -0,0 +1,9 @@
+//That is the first screen that should be displayed.
+
+import PlacesList from "../components/Places/PlacesList";
+
+function AllPlaces() {
+  return <PlacesList />;
+}
+
+export default AllPlaces;
